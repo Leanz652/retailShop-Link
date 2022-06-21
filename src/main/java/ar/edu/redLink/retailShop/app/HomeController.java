473e1638cf855571065491076be7428ec4b2b980
Leanz652 <lean.alvarez@live.com.ar>
2022-06-21 13:32:21 -0300

    se suma home controller

---
+++ src/main/java/ar/edu/redLink/retailShop/app/HomeController.java
@@ -0,0 +1,19 @@
+package ar.edu.redLink.retailShop.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = {"","/home"})
+public class HomeController {
+
+	@GetMapping("/hola")
+	public String hola() {
+		return "hola";
+	}
+
+
+
+
+}
